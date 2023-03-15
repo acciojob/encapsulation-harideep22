fa0d55befa08f143a78be076602c4d0d77d2c6c2
Harideep KS <harideepkshyam2@gmail.com>
2023-03-15 14:53:27 +0530

    Assignment Done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
+
 public class Main {
-  
+  public static void main(String[] args){
+    RWOnly r1=new RWOnly();
+//    r1.name="Hari";
+//    System.out.println(r1.name);  //name has private access in com.driver.RWOnly
+      r1.setters("Hari");
+      System.out.println(r1.getters());
+  }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+    public void setters(String name){
+        this.name=name;
+    }
+    public String getters(){
+        return this.name;
+    }
+
 }
